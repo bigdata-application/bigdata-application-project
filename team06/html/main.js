b11184a4b47dd7dbf09bcacb0a36a4f8043d60d6
MinjaeJo1999 <76865900+MinjaeJo1999@users.noreply.github.com>
2022-11-09 19:24:59 +0900

    [민재] 메인 화면 틀 업로드

---
+++ team06/html/main.js
@@ -0,0 +1,15 @@
+function moveFeature1() {
+	location.href = "feature1.html";
+}
+function moveFeature1Report1() {
+	location.href = "report1.html";
+}
+function moveFeature2() {
+	location.href = "feature2.html";
+}
+function moveFeature2Report2() {
+	location.href = "report2.html";
+}
+function moveFeature3() {
+	location.href = "feature3.html";
+}

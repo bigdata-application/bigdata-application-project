30bcc42dd3287da2a2fff3039be19edf3ecee320
MinjaeJo1999 <76865900+MinjaeJo1999@users.noreply.github.com>
2022-11-11 20:24:43 +0900

    [민재] 로그인 / 회원가입 페이지 js 동작 업데이트

--- team06/html/main.js
+++ team06/html/main.js
@@ -1,15 +1,23 @@
 function moveFeature1() {
 	location.href = "feature1.html";
 }
+
 function moveFeature1Report1() {
 	location.href = "report1.html";
 }
+
 function moveFeature2() {
 	location.href = "feature2.html";
 }
+
 function moveFeature2Report2() {
 	location.href = "report2.html";
 }
+
 function moveFeature3() {
 	location.href = "feature3.html";
+}
+
+function login() {
+	location.href = "index.html";
 }

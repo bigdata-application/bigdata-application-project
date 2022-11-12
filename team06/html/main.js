9e5630e268367efa1cf8a8af7e8f284bb2b03aba
MinjaeJo1999 <76865900+MinjaeJo1999@users.noreply.github.com>
2022-11-12 21:41:58 +0900

    [민재] main.js 경로 수정

--- team06/html/main.js
+++ team06/html/main.js
@@ -1,23 +1,31 @@
 function moveFeature1() {
-	location.href = "feature1.html";
+	document.location.href = "./feature1.html";
 }
 
 function moveFeature1Report1() {
-	location.href = "report1.php";
+	document.location.href = "./report1.php";
 }
 
 function moveFeature2() {
-	location.href = "feature2.html";
+	document.location.href = "./feature2.php";
 }
 
 function moveFeature2Report2() {
-	location.href = "report2.html";
+	document.location.href = "./report2-1.php";
 }
 
 function moveFeature3() {
-	location.href = "feature3.html";
+	document.location.href = "./feature3.php";
 }
 
 function login() {
-	location.href = "index.html";
+	document.location.href = "./index.html";
+}
+
+function moveRangeList() {
+	document.location.href = "./movieListByRange.php";
+}
+
+function moveRangeAndNationList() {
+	document.location.href = "./movieListByRangeAndNation.php";
 }

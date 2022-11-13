c3e8744d716ad520db18e631b868d60f8b47bbfc
yuna kim <81355914+ynk1011@users.noreply.github.com>
2022-11-13 22:19:41 +0900

    Update movieListByGenre.php
    영화 분류, 댓글 표시 기능 완료
    (댓글 작성 미완성)

--- team06/html/movieListByGenre.php
+++ team06/html/movieListByGenre.php
@@ -11,6 +11,9 @@
         session_start();
         $genreOption = $_POST['genre'];
         $_SESSION['genreValue'] = $genreOption;
+        $mysqli= mysqli_connect("localhost", "team06", "team06", "team06");
+
+
     ?>
         <div class = "container">
             <section class="middleBanner">
@@ -19,15 +22,38 @@
             <div class="commentOutputForm">
                 <div class="commentOutputForm2">
                     <!--댓글 출력-->
-                    <div>&nbsp;&nbsp;&nbsp;&nbsp;username1</div>
-                    <div class="commentOutput"> 
-                        <span>comment output from database</span>
-                    </div>
-                    <!--댓글 출력 (하드코딩)-->
-                    <div>&nbsp;&nbsp;&nbsp;&nbsp;username2</div>
-                    <div class="commentOutput"> 
-                        <span>very interesting</span>
-                    </div>
+                    <?php 
+                        //session_start();
+                        //$mysqli= mysqli_connect("localhost", "team06", "", "team06");
+                        $sql= "select * from GENRE_RANKING_COMMENT order by id desc;";
+                        $res=mysqli_query($mysqli,$sql);
+                        while ($rowData= $res->fetch_array()) {
+                            $id=$rowData['id'];
+                            $user_idx=$rowData['user_idx'];
+                            $sql2= "select * from user where user_idx=$user_idx";
+                            $res2=mysqli_query($mysqli,$sql2);
+                            while ($rowData2= $res2->fetch_array()) {
+                                $user_name= $rowData2['user_name'];
+                            }
+                            $content= $rowData['content'];
+
+                            echo "<div>&nbsp;&nbsp;&nbsp;&nbsp;$user_name</div>
+                            <div class='commentOutput'> 
+                                <span>$content</span>
+                               </div>
+                            
+                            ";
+
+                        }
+                       
+                        
+                           
+                        
+                        
+                    ?>
+
+                </div>
+                    
                 </div>
             </div>
             <section class="features">
@@ -42,10 +68,10 @@
                     <form class="selectBox" method="post" action="./movieListByGenreAndNation.php" >
                             <select name="genreNation" class="nationSelect">
                                 <option selected="selected" disabled value="0">Select nation</option>
-                                <option value="Korean">Korea</option>
-                                <option value="American">USA</option>
-                                <option value="Japanese">Japan</option>
-                                <option value="other">etc</option>
+                                <option value="한국">Korea</option>
+                                <option value="미국">USA</option>
+                                <option value="일본">Japan</option>
+                                <option value="기타">etc</option>
                             </select>
                             <div>
                                 <button class="selectButton" type="submit" value="submit" name="submit" onclick=' moveGenreAndNationList()'> > </button>
@@ -56,26 +82,48 @@
                     <div class="poster">
 
                     </div>
-                    <div class="info">
-                        <p class="boldTitle">(관객수) </p>
-                        <p class="infoText">title: </p>
-                        <p class="infoText">nation: </p>
-                        <p class="infoText">genre: </p>
-                        <p class="infoText"> profit: </p>
-                    </div>
+                    <?php
+                        
+                        $sql = "select * from mv_info where genre='".$_SESSION['genreValue']."' order by audience desc;";
+                        $res = mysqli_query($mysqli,$sql);
+                        
+                        while ($movieArray = mysqli_fetch_array($res,MYSQLI_ASSOC)) {
+                            $audience = $movieArray['audience'];
+                            // $movie_name_eng = $movieArray['movie_name_En'];
+                            $movie_name_kor = $movieArray['movie_name_kor'];
+                            $nation = $movieArray['nation'];
+                            $earned_money = $movieArray['earned_money'];
+
+                            echo "<div class='info'>
+                                <p class='boldTitle'>(관객수) $audience </p>
+                                <p class='infoText'>title: $movie_name_kor </p>
+                                <p class='infoText'>nation: $nation </p>
+                                <p class='infoText'>genre: $genreOption </p>
+                                <p class='infoText'> profit: $earned_money </p>
+                                </div>";
+                        }
+                        
+
+
+
+                    ?>
+
+                    
                 </div>
             </section>
             <div class="commentInputForm">
                 <div class="commentInputForm2">
                     <!--댓글 입력-->
                     <div>+comment&nbsp;&nbsp;&nbsp;&nbsp;</div>
-                    <div class="commentInput"> 
-                        <input class="input" type="text" placeholder="enter comment">
-                    </div>
-                    <!--댓글 입력 제출 버튼-->
-                    <div>
-                        <button class="commentButton" type="button"> > </button>
-                    </div>
+                    <form action= "../php/genrerankingcomment.php" METHOD= "post">
+                        <div class="commentInput"> 
+                            <input class="input" name="comment" type="text" placeholder="enter comment">
+                        </div>
+                        <!--댓글 입력 제출 버튼-->
+                        <div>
+                            <button class="commentButton" type="submit"> </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div> 

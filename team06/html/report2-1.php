dd5ea2c988825d6b4dc1b9f9f8389bdf1df69a95
jin033 <76717778+jin033@users.noreply.github.com>
2022-11-14 20:29:21 +0900

    [예진] report2-1.php 수정

--- team06/html/report2-1.php
+++ team06/html/report2-1.php
@@ -11,20 +11,6 @@
             <section class="middleBanner">
                     <span class="title">2022 Korea Box Office Report</span>
             </section>
-            <div class="commentOutputForm">
-                <div class="commentOutputForm2">
-                    <!--댓글 출력-->
-                    <div>&nbsp;&nbsp;&nbsp;&nbsp;username1</div>
-                    <div class="commentOutput"> 
-                        <span>comment output from database</span>
-                    </div>
-                    <!--댓글 출력 (하드코딩)-->
-                    <div>&nbsp;&nbsp;&nbsp;&nbsp;username2</div>
-                    <div class="commentOutput"> 
-                        <span>very interesting</span>
-                    </div>
-                </div>
-            </div>
             <section class="features">
                 <div class="snd_container">
                     <button class="reportButton1" type="button" onclick='moveFeature1Report1()'>
@@ -59,33 +45,14 @@
                         </div>
                     </form>
 
-                    <div class="movieInfoBox">
-                    <div class="poster">
+                         
 
-                    </div>
-                    <div class="info">
-                        <p class="boldTitle">(관객수) </p>
-                        <p class="infoText">title: </p>
-                        <p class="infoText">nation: </p>
-                        <p class="infoText">genre: </p>
-                        <p class="infoText"> profit: </p>
-                    </div>
-                </div>
-                             
+      
+
+                
+
+    
             </section>
-            <div class="commentInputForm">
-                <div class="commentInputForm2">
-                    <!--댓글 입력-->
-                    <div>+comment&nbsp;&nbsp;&nbsp;&nbsp;</div>
-                    <div class="commentInput"> 
-                        <input class="input" type="text" placeholder="enter comment">
-                    </div>
-                    <!--댓글 입력 제출 버튼-->
-                    <div>
-                        <button class="commentButton" type="button"> > </button>
-                    </div>
-                </div>
-            </div>
   
         </div> 
     </body>

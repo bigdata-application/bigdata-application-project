04d0322ab1e3eec366a10b7ab0a3a2f6cc5eb7d5
jin033 <76717778+jin033@users.noreply.github.com>
2022-11-14 22:16:58 +0900

    [예진]movieListByRangeAndNation.php 수정

--- team06/html/movieListByRangeAndNation.php
+++ team06/html/movieListByRangeAndNation.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
         <link href="./css/style.css" rel="stylesheet" />
         <script type="text/javascript" src="./main.js"></script>
@@ -10,8 +11,12 @@
     <?php
         session_start();
         $passRange = $_SESSION['rangeValue'];
-        //$rangeOption = $_POST['passRange'];
-        $nationOption = $_POST['nation']
+        $rangeOption= $passRange; 
+
+        $nationOption = $_POST['nationValue'];
+    
+        
+
     ?>
         <div class = "container">
             <section class="middleBanner">
@@ -19,18 +24,54 @@
             </section>
             <div class="commentOutputForm">
                 <div class="commentOutputForm2">
-                    <!--댓글 출력-->
-                    <div>&nbsp;&nbsp;&nbsp;&nbsp;username1</div>
-                    <div class="commentOutput"> 
-                        <span>comment output from database</span>
-                    </div>
-                    <!--댓글 출력 (하드코딩)-->
-                    <div>&nbsp;&nbsp;&nbsp;&nbsp;username2</div>
-                    <div class="commentOutput"> 
-                        <span>very interesting</span>
-                    </div>
+                <?php
+                        session_start();
+                        $mysqli = mysqli_connect("localhost", "team06", "team06", "team06");
+                        $sql = "select * from audience_range_nation_comment order by id desc;";
+                        $result=mysqli_query($mysqli,$sql);
+                        while ($rowData= $result->fetch_array()) {
+                            $id=$rowData['id'];
+                            $user_idx=$rowData['user_idx'];
+                            $sql2= "select * from user where user_idx=$user_idx";
+                            $result2=mysqli_query($mysqli,$sql2);
+                            while ($rowData2= $result2->fetch_array()) {//username추출
+                                $user_name= $rowData2['user_name'];
+                            }
+                            $content= $rowData['content'];
+
+                            if($_SESSION['user_idx']==$user_idx) {
+                            echo "<div>&nbsp;&nbsp;&nbsp;&nbsp;$user_name</div>
+                            <div class='commentOutput'> 
+                                <span>$content</span>
+                                    <form action='../php/rankbynumnationcommentdelete.php' method='post' style='display: inline;'>
+                                <button class='commentButton' type='submit' style='color: #FF92B1;' name='delete' value=$id> x </button>
+                                    </form>
+                            <br>
+                                    <form action= '../php/rankbynumnationcommentmodify.php' method= 'post' style='display: inline;' >
+                                <div class='commentInput' style='margin-left: -8px; padding: -8px;'> 
+                                  <input class='input' name= 'modify' type='text' placeholder='enter comment'>
+                                </div>
+                                  <input type='hidden' name='id' value=$id>                      
+                                <button class='commentButton' type='submit' style='color: #C8FAC8;' style='display: inline;'> modify </button>
+                                    </form>
+                            
+                            </div>
+                               
+                            ";
+                        }
+                        else {
+                            echo "<div>&nbsp;&nbsp;&nbsp;&nbsp;$user_name</div>
+                            <div class='commentOutput'> 
+                                <span>$content</span>
+                               </div>";
+                        }
+                    }
+                    ?>    
+                
                 </div>
             </div>
+
+
             <section class="features">
                 <div class="snd_container">
                     <button class="reportButton1" type="button" onclick='moveFeature1Report1()'>
@@ -47,32 +88,81 @@
                     </button>
                     </div>
 
-                <div class="movieInfoBox">
-                    <div class="poster">
+                <?php
+                        header('Content-Type: text/html; charset=utf-8');
+                        $mysqli = mysqli_connect("localhost", "team06", "team06", "team06");
 
-                    </div>
-                    <div class="info">
-                        <p class="boldTitle">(관객수) </p>
-                        <p class="infoText">title: </p>
-                        <p class="infoText">nation: </p>
-                        <p class="infoText">genre: </p>
-                        <p class="infoText"> profit: </p>
-                    </div>
-                </div>
+                        if(mysqli_connect_error()){
+                          printf("Conncet failed: %s\n", mysqli_connect_error());
+                          exit();
+                        }else{
+                          $condition = "";
+                          if ($rangeOption == 'over 10 million') $condition = "audience >= 10000000";
+                          if ($rangeOption == '5 ~ 10 million') $condition = "audience < 10000000 and audience >= 5000000";
+                          if ($rangeOption == '1 ~ 5 million') $condition = "audience < 5000000 and audience >= 1000000"; 
+                          if ($rangeOption == 'under 1 million') $condition = "audience < 1000000";
+
+                          $nation_condition = "";
+                          if ($nationOption == 'Korean') $nation_condition = "nation = '한국'";
+                          if ($nationOption == 'American') $nation_condition = "nation = '미국'";
+                          if ($nationOption == 'Japanese') $nation_condition = "nation = '일본'";
+                          if ($nationOption == 'other') $nation_condition = "nation = '기타'";
+
+                          $sql = "SELECT audience, movie_name_kor, nation, genre, earned_money 
+                              from mv_info where $condition and $nation_condition order by audience desc";
+                              $res = mysqli_query($mysqli, $sql);
+                              
+                              if($res){
+                                while($result = mysqli_fetch_array($res, MYSQLI_ASSOC)){
+                                  $audience = $result['audience'];
+                                  $movie_name_kor = $result['movie_name_kor'];
+                                  $nation = $result['nation'];
+                                  $genre = $result['genre'];
+                                  $earned_money = $result['earned_money'];
+
+                                  echo '<div class = "movieInfoBox">';
+                                  //echo '<div class = "info">';
+
+                                  echo '<div class = "poster">';
+
+                                  echo '</div>';
+
+                                  echo '<div class = "info">';
+                                  echo '<p class="infoText">(관객 수):' .$audience.' </p>';
+                                  echo '<p class="infoText">title:' .$movie_name_kor.' </p>';
+                                  echo '<p class="infoText">nation:' .$nation.' </p>';
+                                  echo '<p class="infoText">genre: '.$genre.' </p>';
+                                  echo '<p class="infoText">profit: '.$earned_money.' </p>';
+                                  echo '</div>';
+                                  
+                                  echo '</br>';
+
+                                  //echo '</div>'; //info 닫기
+                                  echo '</div>'; //movieInfoBox 닫기
+                                }
+                              }
+                        }
+                        
+                      ?>
             </section>
             <div class="commentInputForm">
                 <div class="commentInputForm2">
+
                     <!--댓글 입력-->
                     <div>+comment&nbsp;&nbsp;&nbsp;&nbsp;</div>
+                    <form action= "../php/rankbynumnationcomment.php" METHOD= "post">
                     <div class="commentInput"> 
-                        <input class="input" type="text" placeholder="enter comment">
+                        <input class="input" name = "comment" type="text" placeholder="enter comment">
                     </div>
                     <!--댓글 입력 제출 버튼-->
                     <div>
-                        <button class="commentButton" type="button"> > </button>
+                        <button class="commentButton" type="submit"> > </button>
                     </div>
+                    </form>
+                    
                 </div>
             </div>
+
         </div> 
     </body>
     </html>

a5897bf51644cdfa8db56aaea10787a13af46da0
jin033 <76717778+jin033@users.noreply.github.com>
2022-11-14 20:41:04 +0900

    [예진]report2-1 수정

--- team06/html/report2-1.php
+++ team06/html/report2-1.php
@@ -11,6 +11,57 @@
             <section class="middleBanner">
                     <span class="title">2022 Korea Box Office Report</span>
             </section>
+            <div class="commentOutputForm">
+                <div class="commentOutputForm2">
+                    <!--댓글 출력-->
+
+                    <?php //db로부터 댓글 가져오기
+                        session_start();
+                        $mysqli= mysqli_connect("localhost", "team06", "team06", "team06");
+                        $sql= "select * from audience_comment order by id desc;";//id 내림차순으로 최신순으로 정렬
+                        $result=mysqli_query($mysqli,$sql);
+                        while ($rowData= $result->fetch_array()) {
+                            $id=$rowData['id'];
+                            $user_idx=$rowData['user_idx'];
+                            $sql2= "select * from user where user_idx=$user_idx";
+                            $result2=mysqli_query($mysqli,$sql2);
+                            while ($rowData2= $result2->fetch_array()) {//username추출
+                                $user_name= $rowData2['user_name'];
+                            }
+                            $content= $rowData['content'];
+
+                            if($_SESSION['user_idx']==$user_idx) {
+                            echo "<div>&nbsp;&nbsp;&nbsp;&nbsp;$user_name</div>
+                            <div class='commentOutput'> 
+                                <span>$content</span>
+                                    <form action='../php/audiencecommentdelete.php' method='post' style='display: inline;'>
+                                <button class='commentButton' type='submit' style='color: #FF92B1;' name='delete' value=$id> x </button>
+                                    </form>
+                            <br>
+                                    <form action= '../php/audiencecommentmodify.php' method= 'post' style='display: inline;' >
+                                <div class='commentInput' style='margin-left: -8px; padding: -8px;'> 
+                                    <input class='input' name= 'modify' type='text' placeholder='enter comment'>
+                                </div>          
+                                    <input type='hidden' name='id' value=$id>                      
+                                <button class='commentButton' type='submit' style='color: #C8FAC8;' style='display: inline;'> modify </button>
+                                    </form>
+                            </div>
+                               
+                            ";
+                        }
+                        else {
+                            echo "<div>&nbsp;&nbsp;&nbsp;&nbsp;$user_name</div>
+                            <div class='commentOutput'> 
+                                <span>$content</span>
+                               </div>
+                            
+                            ";
+                        }
+                        }
+                    ?>
+
+                </div>
+            </div>
             <section class="features">
                 <div class="snd_container">
                     <button class="reportButton1" type="button" onclick='moveFeature1Report1()'>
@@ -24,14 +75,54 @@
                         report <br/> by film
                     </button>
                 </div>
-               
-                <!--아래 selectBox 선택값 다음 페이지에 전달하는 php 코드
-                    <form action="rangeSelect.php" method="post">
-                    </form>
-                    //rangeSelect.php -> 목록 출력 / 타이틀 value 값으로 고정
-                    $range = $_POST["range"]; 
-                -->
                 
+                <?php
+                        $mysqli = mysqli_connect("localhost", "team06", "team06", "team06");
+
+                        if(mysqli_connect_error()){
+                          printf("Conncet failed: %s\n", mysqli_connect_error());
+                          exit();
+                        }else{
+
+                          $sql = "SELECT audience, movie_name_kor, nation, genre, earned_money 
+                              from mv_info order by audience desc";
+                              $res = mysqli_query($mysqli, $sql);
+                              
+                              if($res){
+                                while($result = mysqli_fetch_array($res, MYSQLI_ASSOC)){
+                                  $audience = $result['audience'];
+                                  $movie_name_kor = $result['movie_name_kor'];
+                                  $nation = $result['nation'];
+                                  $genre = $result['genre'];
+                                  $earned_money = $result['earned_money'];
+
+                                  echo '<div class = "movieInfoBox">';
+                                  //echo '<div class = "info">';
+
+                                  echo '<div class = "poster">';
+
+                                  echo '</div>';
+
+                                  echo '<div class = "info">';
+                                  echo '<p class="infoText">(관객 수):' .$audience.' </p>';
+                                  echo '<p class="infoText">title:' .$movie_name_kor.' </p>';
+                                  echo '<p class="infoText">nation:' .$nation.' </p>';
+                                  echo '<p class="infoText">genre: '.$genre.' </p>';
+                                  echo '<p class="infoText">profit: '.$earned_money.' </p>';
+                                  echo '</div>';
+                                  
+                                  echo '</br>';
+
+                                  //echo '</div>'; //info 닫기
+                                  echo '</div>'; //movieInfoBox 닫기
+                                }
+                              }
+                        }
+                        
+                ?>
+                
+
+
                     <form class="selectBox" method="post" action="./movieListByRange.php" >
                         <select name="audRange" class="rangeSelect">
                             <option selected="selected" disabled value="0">Select range</option>
@@ -45,15 +136,26 @@
                         </div>
                     </form>
 
-                         
 
-      
-
-                
-
-    
             </section>
   
+            <div class="commentInputForm">
+                <div class="commentInputForm2">
+                    <!--댓글 입력-->
+                          
+
+                    <div>+comment&nbsp;&nbsp;&nbsp;&nbsp;</div>
+                        <form action= "../php/audiencecomment.php" METHOD= "post">
+                    <div class="commentInput"> 
+                        <input class="input" name= "comment" type="text" placeholder="enter comment">
+                    </div>
+                    <!--댓글 입력 제출 버튼-->
+                    <div>
+                        <button class="commentButton" type="submit"> > </script> </button> 
+                    </div>
+                        </form>
+                </div>
+            </div>
         </div> 
     </body>
     </html>
